b989a3046cda41ccaca6099f2c2afd7d1f91305f
Ziru Zou <jiyucoding@yahoo.com>
2023-02-04 11:34:41 -0500

    call CB passed down from App when onClick

--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,12 @@ import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  return <div className="child"
+              style={{ backgroundColor: "#FFF" }} 
+
+              // 3. onClick event listener, pass in prop passed down from Parent 
+              onClick={onChangeColor}
+          />;
 }
 
 export default Child;
